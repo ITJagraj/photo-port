b928374cb475ecbfadd970bad3bd9944c153f4c7
Jagraj Singh <singh.jagraj96@yahoo.com>
2021-11-23 13:10:21 -0500

    another component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Nav from './components/Nav';
 import About from './component/About'
 
 function App() {
   return (
     <div className="App">
+      <Nav>
+      </Nav>
      <main>
        <About>
        </About>
