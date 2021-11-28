16b04ecccea6b438c2809cce62ad36986918ac8a
Jagraj Singh <singh.jagraj96@yahoo.com>
2021-11-28 00:58:19 -0500

    jest test

--- src/component/Nav/index.js
+++ src/component/Nav/index.js
@@ -1,56 +1,58 @@
-import React from 'react';
+import React from "react";
 
 function Nav() {
+  const categories = [
+    {
+      name: "commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    {
+      name: "portraits",
+      description: "Portraits of people in my life",
+    },
+    {
+      name: "food",
+      description: "Delicious delicacies",
+    },
+    {
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
+  ];
 
-    const categories = [
-        {
-            name: "commercial",
-            description: "Photos of grocery stores, food trucks, and other commercial projects",
-        },
-        {
-            name: "portraits", description: "Portraits of people in my life" },
-        {
-            name: "food", description: "Delicious delicacies"     
-        },
-        {
-            name: "landscape",
-            description: "Fields, farmhouses, waterfalls, and the beauty of nature",   
-        },
-    ];
-
-    function categorySelected(name) {
-        console.log(`${name} clicked`)
-    }
+  function categorySelected(name) {
+    console.log(`${name} clicked`);
+  }
 
   return (
-    <header className= "flex-row px-1">
-        <h2>
+    <header className="flex-row px-1">
+      <h2>
         <a data-testid="link" href="/">
-        <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+          <span role="img" aria-label="camera">
+            {" "}
+            📸
+          </span>{" "}
+          Oh Snap!
         </a>
-        </h2>
-        <nav>
-            <ul className="flex-row">
-                <li className= "mx-2">
-                
-                </li>
-                <li>
-                <a data-testid="about" href="#about">
-                    <span>Contact</span>
-                    </a>
-                </li>
-                {categories.map((category) => (
-                    <li 
-                    className="mx-1" 
-                    key={category.name}
-                    >
-                        <span onClick={() => categorySelected(category.name)} >
-                            {category.name}
-                        </span>
-                    </li>
-                ))}
-            </ul>
-        </nav>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2"></li>
+          <li>
+            <a data-testid="about" href="#about">
+              <span>About me</span>
+            </a>
+          </li>
+          {categories.map((category) => (
+            <li className="mx-1" key={category.name}>
+              <span onClick={() => categorySelected(category.name)}>
+                {category.name}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 }
