e0b14f260912cfc4aed070011a879c39e398eb9f
Jagraj Singh <singh.jagraj96@yahoo.com>
2021-12-06 16:39:35 -0500

    useEffect hook

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,27 +1,13 @@
-import React, { useState } from "react";
-import { capitalizeFirstLetter } from "../../utils/helpers.js";
+import React from "react";
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
-function Nav() {
-  const [currentCategory, setCurrentCategory] = useState(categories[0]);
-  const [categories] = useState([
-    {
-      name: "commercial",
-      description:
-        "Photos of grocery stores, food trucks, and other commercial projects",
-    },
-    {
-      name: "portraits",
-      description: "Portraits of people in my life",
-    },
-    {
-      name: "food",
-      description: "Delicious delicacies",
-    },
-    {
-      name: "landscape",
-      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-    },
-  ]);
+function Nav(props) {
+  const { categories = [], setCurrentCategory, currentCategory } = props;
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
+
 
   return (
     <header className="flex-row px-1">
@@ -37,9 +23,11 @@ function Nav() {
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a href="#about">About me</a>
+            <a data-testid="about" href="#about">
+              About me
+            </a>
           </li>
-          <li>
+          <li className="mx-2">
             <span>Contact</span>
           </li>
           {categories.map((category) => (
